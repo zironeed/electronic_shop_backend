2167e5381c836eeb20e9932763010cd99a72ea37
Adil S <forever@nzironeed.ru>
2023-10-08 20:07:01 +0500

    urls for Seller

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
+from shop.views import SellerCreateView, SellerDestroyView, SellerUpdateView, SellerListView, SellerRetrieveView
+
 from shop.apps import ShopConfig
 
 app_name = ShopConfig.name
 
 urlpatterns = [
-
+    path('create/', SellerCreateView.as_view(), name='seller_create'),
+    path('update/', SellerUpdateView.as_view(), name='seller_update'),
+    path('retrieve/', SellerRetrieveView.as_view(), name='seller_retrieve'),
+    path('destroy/', SellerDestroyView.as_view(), name='seller_destroy'),
+    path('list/', SellerListView.as_view(), name='seller_list'),
 ]
