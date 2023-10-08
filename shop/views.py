6e443964549b502a639ec2007da5e0c1957369a8
Adil S <forever@nzironeed.ru>
2023-10-08 18:34:03 +0500

    CRUD for Seller

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
 
-# Create your views here.
+from shop.models import Seller, Contact, Product
+from shop.serializers import SellerSerializer, ContactSerializer, ProductSerializer
+
+
+class SellerListView(ListAPIView):
+    serializer_class = SellerSerializer
+    queryset = Seller.objects.all()
+
+
+class SellerCreateView(CreateAPIView):
+    serializer_class = SellerSerializer
+    queryset = Seller.objects.all()
+
+
+class SellerRetrieveView(RetrieveAPIView):
+    serializer_class = SellerSerializer
+    queryset = Seller.objects.all()
+
+
+class SellerUpdateView(UpdateAPIView):
+    serializer_class = SellerSerializer
+    queryset = Seller.objects.all()
+
+
+class SellerDestroyView(DestroyAPIView):
+    serializer_class = SellerSerializer
+    queryset = Seller.objects.all()
