154f7901885f568987dfa84267b4b66851ce9fdc
Adil S <forever@nzironeed.ru>
2023-10-10 17:50:02 +0500

    update urls

--- shop/urls.py
+++ shop/urls.py
@@ -9,12 +9,12 @@ app_name = ShopConfig.name
 
 urlpatterns = [
     path('create/', SellerCreateView.as_view(), name='seller_create'),
-    path('update/', SellerUpdateView.as_view(), name='seller_update'),
-    path('retrieve/', SellerRetrieveView.as_view(), name='seller_retrieve'),
-    path('destroy/', SellerDestroyView.as_view(), name='seller_destroy'),
+    path('update/<int:pk>/', SellerUpdateView.as_view(), name='seller_update'),
+    path('retrieve/<int:pk>/', SellerRetrieveView.as_view(), name='seller_retrieve'),
+    path('destroy/<int:pk>/', SellerDestroyView.as_view(), name='seller_destroy'),
     path('list/', SellerListView.as_view(), name='seller_list'),
 
     path('create/contact/', ContactCreateView.as_view(), name='contact_create'),
-    path('update/contact/', ContactUpdateView.as_view(), name='contact_update'),
-    path('retrieve/contact/', ContactRetrieveView.as_view(), name='contact_retrieve'),
+    path('update/contact/<int:pk>/', ContactUpdateView.as_view(), name='contact_update'),
+    path('retrieve/contact/<int:pk>/', ContactRetrieveView.as_view(), name='contact_retrieve'),
 ]
