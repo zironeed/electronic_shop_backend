6a5519ce01670361407176afaa294520e80a2bad
Adil S <forever@nzironeed.ru>
2023-10-10 09:22:16 +0500

    add users urls

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls', namespace='shop')),
-    # path('users/', include('users.urls', namespace='users')),
+    path('users/', include('users.urls', namespace='users')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
