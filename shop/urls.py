a6a4ee05eb85bfdc172ed93ea42e11bc41031597
Adil S <forever@nzironeed.ru>
2023-10-10 17:48:46 +0500

    Contact views and urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from shop.views import SellerCreateView, SellerDestroyView, SellerUpdateView, SellerListView, SellerRetrieveView
+from shop.views import SellerCreateView, SellerDestroyView, SellerUpdateView, SellerListView, SellerRetrieveView, \
+    ContactCreateView, ContactUpdateView, ContactRetrieveView
 
 from shop.apps import ShopConfig
 
@@ -12,4 +13,8 @@ urlpatterns = [
     path('retrieve/', SellerRetrieveView.as_view(), name='seller_retrieve'),
     path('destroy/', SellerDestroyView.as_view(), name='seller_destroy'),
     path('list/', SellerListView.as_view(), name='seller_list'),
+
+    path('create/contact/', ContactCreateView.as_view(), name='contact_create'),
+    path('update/contact/', ContactUpdateView.as_view(), name='contact_update'),
+    path('retrieve/contact/', ContactRetrieveView.as_view(), name='contact_retrieve'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -27,3 +27,18 @@ class SellerUpdateView(UpdateAPIView):
 class SellerDestroyView(DestroyAPIView):
     serializer_class = SellerSerializer
     queryset = Seller.objects.all()
+
+
+class ContactCreateView(CreateAPIView):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
+
+
+class ContactRetrieveView(RetrieveAPIView):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
+
+
+class ContactUpdateView(UpdateAPIView):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
